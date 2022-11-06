4458ee75a04f04b24a4671414a6a29e02d33204a
ivan117-sys <ivanmuskovic@gmail.com>
2022-11-06 11:23:33 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9fb74bf7-28da-4f75-91de-1097890fb503';
+export const MODAL_CLOSE_SEC = 2.5;
